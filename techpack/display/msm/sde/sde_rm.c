83d0fe72189171e3f90a94ec9241e6dcd75e151f
tosasitill <lfym888888@gmail.com>
2026-06-13 11:48:44 +0800

    display: fix resource manager warning fallout

--- techpack/display/msm/sde/sde_rm.c
+++ techpack/display/msm/sde/sde_rm.c
@@ -918,13 +918,13 @@ static bool _sde_rm_reserve_pp(
 		struct sde_rm_rsvp *rsvp,
 		struct sde_rm_requirements *reqs,
 		const struct sde_lm_cfg *lm_cfg,
-		const struct sde_pingpong_cfg *pp_cfg,
 		struct sde_rm_hw_blk *lm,
 		struct sde_rm_hw_blk **dspp,
 		struct sde_rm_hw_blk **ds,
 		struct sde_rm_hw_blk **pp)
 {
 	struct sde_rm_hw_iter iter;
+	const struct sde_pingpong_cfg *pp_cfg;
 
 	sde_rm_init_hw_iter(&iter, 0, SDE_HW_BLK_PINGPONG);
 	while (_sde_rm_get_hw_locked(rm, &iter)) {
@@ -987,7 +987,6 @@ static bool _sde_rm_check_lm_and_get_connected_blks(
 		u32 conn_lm_mask)
 {
 	const struct sde_lm_cfg *lm_cfg = to_sde_hw_mixer(lm->hw)->cap;
-	const struct sde_pingpong_cfg *pp_cfg;
 	bool ret, is_conn_primary, is_conn_secondary;
 	u32 lm_primary_pref, lm_secondary_pref, cwb_pref;
 
@@ -1073,7 +1072,7 @@ static bool _sde_rm_check_lm_and_get_connected_blks(
 		return ret;
 
 	/* Reserve pp */
-	ret = _sde_rm_reserve_pp(rm, rsvp, reqs, lm_cfg, pp_cfg, lm,
+	ret = _sde_rm_reserve_pp(rm, rsvp, reqs, lm_cfg, lm,
 			dspp, ds, pp);
 	if (!ret)
 		return ret;
@@ -1353,11 +1352,9 @@ static bool _sde_rm_check_vdc(struct sde_rm *rm,
 		struct sde_rm_rsvp *rsvp,
 		struct sde_rm_hw_blk *vdc)
 {
-	const struct sde_vdc_cfg *vdc_cfg = to_sde_hw_vdc(vdc->hw)->caps;
-
 	/* Already reserved? */
 	if (RESERVED_BY_OTHER(vdc, rsvp)) {
-		SDE_DEBUG("vdc %d already reserved\n", vdc_cfg->id);
+		SDE_DEBUG("vdc %d already reserved\n", vdc->id);
 		return false;
 	}
 
